cc59f572a74d6d238625e3b1e770fce38f735a3f
kjlundsgaard <katie.lundsgaard@gmail.com>
2016-06-04 20:27:33 -0700

    finished sorting methods

--- recursion.py
+++ recursion.py
@@ -3,6 +3,8 @@
 # --------- #
 
 # 1. Write a function that uses recursion to print each item in a list.
+
+
 def print_item(my_list, i=0):
     """Prints each item in a list recursively.
 
@@ -12,7 +14,12 @@ def print_item(my_list, i=0):
         3
 
     """
-    pass
+
+    if i == len(my_list):
+        return
+
+    print my_list[i]
+    print_item(my_list, i+1)
 
 
 # 2. Write a function that uses recursion to print each node in a tree.
@@ -39,7 +46,14 @@ def print_all_tree_data(tree):
         3
 
     """
-    pass
+
+    print tree.data
+
+    if not tree.children:
+        return
+
+    for child in tree.children:
+        print_all_tree_data(child)
 
 # 3. Write a function that uses recursion to find the length of a list.
 
@@ -50,7 +64,11 @@ def list_length(my_list):
         4
 
     """
-    pass
+
+    if my_list == []:
+        return 0
+
+    return 1 + list_length(my_list[1:])
 
 
 # 4. Write a function that uses recursion to count how many nodes are in a tree.
@@ -74,7 +92,13 @@ def num_nodes(tree):
         3
     """
 
-    pass
+    if not tree.children:
+        return 1
+
+    count = 1
+
+    for child in tree.children:
+        return count + num_nodes(child)
 
 #####################################################################
 # END OF ASSIGNMENT: You can ignore everything below.
--- sorting.py
+++ sorting.py
@@ -1,12 +1,27 @@
 #Sorting
 
+
 def bubble_sort(lst):
     """returns a sorted list using a optimized bubble sort algorithm
     i.e. using a variable to track if there hasn't been a swap
         >>> bubble_sort([3, 5, 7, 2, 4, 1])
         [1, 2, 3, 4, 5, 7]
     """
-    pass
+    # go through list as many times as there are items in list so you go back and check every item
+    for i in range(len(lst)):
+        # set swap to False where a swap has not yet been made, will change when swap is made
+        swap = False
+        # go through list until the last item
+        for j in range(len(lst) - 1):
+            # swap position if one is higher
+            if lst[j] > lst[j + 1]:
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
+                swap = True
+        # if no swap has happened, it's already sorted, so we don't need to revisit
+        if not swap:
+            break
+
+    return lst
 
 
 def merge_lists(list1, list2):
@@ -16,10 +31,26 @@ def merge_lists(list1, list2):
     [1, 3, 4, 7, 9, 11]
     """
 
-    pass
+    # go through lists, compare list1[0] to list2[0] and pop item from whichever is less, put in new list
+    new_list = []
+    while len(list1) != 0 or len(list2) != 0:
+        if not list1:
+            new_list.append(list2.pop(0))
 
+        elif not list2:
+            new_list.append(list1.pop(0))
+
+        elif list1[0] < list2[0]:
+            new_list.append(list1.pop(0))
+
+        else:
+            new_list.append(list2.pop(0))
+
+    return new_list
 
 ##########ADVANCED##########
+
+
 def merge_sort(lst):
     """
     Given a list, returns a sorted version of that list.
@@ -31,10 +62,30 @@ def merge_sort(lst):
     >>> merge_sort([6, 2, 3, 9, 0, 1])
     [0, 1, 2, 3, 6, 9]
     """
-    pass
 
+    if len(lst) == 1:
+        return lst
 
+    mid = len(lst) / 2
 
+    list1 = merge_sort(lst[:mid])
+    list2 = merge_sort(lst[mid:])
+
+    new_list = []
+    while len(list1) != 0 or len(list2) != 0:
+        if not list1:
+            new_list.append(list2.pop(0))
+
+        elif not list2:
+            new_list.append(list1.pop(0))
+
+        elif list1[0] < list2[0]:
+            new_list.append(list1.pop(0))
+
+        else:
+            new_list.append(list2.pop(0))
+
+    return new_list
 
 #####################################################################
 # END OF ASSIGNMENT: You can ignore everything below.
